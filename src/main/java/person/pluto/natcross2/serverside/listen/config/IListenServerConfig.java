5cb6513ee46bef7c8c753a63573012d1eda0990b
Pluto <wangmin07@hotmail.com>
2021-06-30 18:02:49 +0800

    去除无用的默认实现

--- src/main/java/person/pluto/natcross2/serverside/listen/config/IListenServerConfig.java
+++ src/main/java/person/pluto/natcross2/serverside/listen/config/IListenServerConfig.java
@@ -22,64 +22,62 @@ import person.pluto.natcross2.serverside.listen.control.IControlSocket;
  */
 public interface IListenServerConfig {
 
-    /**
-     * 获取监听的端口
-     * 
-     * @author Pluto
-     * @since 2020-01-08 16:51:17
-     * @return
-     */
-    Integer getListenPort();
+	/**
+	 * 获取监听的端口
+	 * 
+	 * @author Pluto
+	 * @since 2020-01-08 16:51:17
+	 * @return
+	 */
+	Integer getListenPort();
 
-    /**
-     * 新建无效端口处理线程
-     * 
-     * @author Pluto
-     * @since 2020-01-08 16:51:26
-     * @param serverListenThread
-     * @return
-     */
-    IClearInvalidSocketPartThread newClearInvalidSocketPartThread(ServerListenThread serverListenThread);
+	/**
+	 * 新建无效端口处理线程
+	 * 
+	 * @author Pluto
+	 * @since 2020-01-08 16:51:26
+	 * @param serverListenThread
+	 * @return
+	 */
+	IClearInvalidSocketPartThread newClearInvalidSocketPartThread(ServerListenThread serverListenThread);
 
-    /**
-     * 创建隧道伙伴
-     * 
-     * @author Pluto
-     * @since 2020-01-08 16:51:41
-     * @param serverListenThread
-     * @return
-     */
-    AbsSocketPart newSocketPart(ServerListenThread serverListenThread);
+	/**
+	 * 创建隧道伙伴
+	 * 
+	 * @author Pluto
+	 * @since 2020-01-08 16:51:41
+	 * @param serverListenThread
+	 * @return
+	 */
+	AbsSocketPart newSocketPart(ServerListenThread serverListenThread);
 
-    /**
-     * 获取字符集
-     * 
-     * @author Pluto
-     * @since 2020-01-08 16:51:57
-     * @return
-     */
-    Charset getCharset();
+	/**
+	 * 获取字符集
+	 * 
+	 * @author Pluto
+	 * @since 2020-01-08 16:51:57
+	 * @return
+	 */
+	Charset getCharset();
 
-    /**
-     * 新建控制器
-     * 
-     * @author Pluto
-     * @since 2020-01-08 16:52:05
-     * @param socketChannel
-     * @param config
-     * @return
-     */
-    IControlSocket newControlSocket(Socket socket, JSONObject config);
+	/**
+	 * 新建控制器
+	 * 
+	 * @author Pluto
+	 * @since 2020-01-08 16:52:05
+	 * @param socketChannel
+	 * @param config
+	 * @return
+	 */
+	IControlSocket newControlSocket(Socket socket, JSONObject config);
 
-    /**
-     * 创建监听端口
-     * 
-     * @author Pluto
-     * @since 2020-01-09 13:24:13
-     * @return
-     * @throws Exception
-     */
-    default public ServerSocket createServerSocket() throws Exception {
-        return new ServerSocket(this.getListenPort());
-    }
+	/**
+	 * 创建监听端口
+	 * 
+	 * @author Pluto
+	 * @since 2020-01-09 13:24:13
+	 * @return
+	 * @throws Exception
+	 */
+	public ServerSocket createServerSocket() throws Exception;
 }
