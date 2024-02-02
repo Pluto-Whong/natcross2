04f1827d244c1ea0a2c7e05bb47907f3ba256bee
Pluto <wangmin07@hotmail.com>
2024-02-02 16:37:35 +0800

    Feature 调整心跳检查机制，记录服务端心跳回复
    取消心跳处的重置时间设定

--- src/main/java/person/pluto/natcross2/clientside/handler/ServerHeartHandler.java
+++ src/main/java/person/pluto/natcross2/clientside/handler/ServerHeartHandler.java
@@ -1,5 +1,6 @@
 package person.pluto.natcross2.clientside.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import person.pluto.natcross2.clientside.adapter.IClientAdapter;
 import person.pluto.natcross2.model.InteractiveModel;
 import person.pluto.natcross2.model.enumeration.InteractiveTypeEnum;
@@ -13,6 +14,7 @@ import person.pluto.natcross2.model.enumeration.NatcrossResultEnum;
  * @author Pluto
  * @since 2020-04-15 13:02:09
  */
+@Slf4j
 public class ServerHeartHandler implements IClientHandler<InteractiveModel, InteractiveModel> {
 
     public static final ServerHeartHandler INSTANCE = new ServerHeartHandler();
@@ -22,16 +24,13 @@ public class ServerHeartHandler implements IClientHandler<InteractiveModel, Inte
                         IClientAdapter<? extends InteractiveModel, ? super InteractiveModel> clientAdapter) throws Exception {
         InteractiveTypeEnum interactiveTypeEnum = InteractiveTypeEnum.getEnumByName(model.getInteractiveType());
         if (InteractiveTypeEnum.HEART_TEST.equals(interactiveTypeEnum)) {
-            clientAdapter.resetServerHeartLastRecvTime();
-
             InteractiveModel sendModel = InteractiveModel.of(model.getInteractiveSeq(), InteractiveTypeEnum.HEART_TEST_REPLY,
                     NatcrossResultEnum.SUCCESS.toResultModel());
             clientAdapter.getSocketChannel().writeAndFlush(sendModel);
 
             return true;
         } else if (InteractiveTypeEnum.HEART_TEST_REPLY.equals(interactiveTypeEnum)) {
-            clientAdapter.resetServerHeartLastRecvTime();
-
+            log.debug("HEART_TEST_REPLY ignore");
             return true;
         }
 
